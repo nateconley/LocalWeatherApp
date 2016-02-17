d82394fb47a54ddb04f50774f92c45c28c4332db
Nathan Conley <nateconley123@gmail.com>
2016-02-17 11:32:39 -0800

    added temperature unit converter

--- public/scripts/weather.js
+++ public/scripts/weather.js
@@ -1,8 +1,13 @@
 angular.module('weatherApp', [])
 .controller('location', function($scope, $http){
+	// Temperature unit button classes
+	$scope.fahrenheitClass = "btn btn-primary";
+	$scope.celciusClass = "btn btn default";
 
 	// Temperature unit to fahrenheit
 	var tempUnit = "F";
+	// Set Temperature for manipulating when unit is changed
+	var temp;
 
 	// handles weather requests
 	$scope.getWeather = function(type){
@@ -17,7 +22,7 @@ angular.module('weatherApp', [])
 		if (type == "zip") {
 			location.zip = $scope.zipCode;
 			// Check if zip is valid (eg: 55555)
-			if (!location.zip.match(/[0-9]{5}/)) {
+			if (!location.zip.match(/[0-9]{5}/) || location.zip.length !== 5) {
 				$scope.invalidZip = true;
 				$scope.loading = false;
 			} else {
@@ -27,6 +32,7 @@ angular.module('weatherApp', [])
 						// Success
 						$scope.city = weatherData.city;
 						$scope.temperature = weatherData.temperature + "º " + tempUnit;
+						temp = weatherData.temperature;
 						$scope.condition = weatherData.condition;
 						// Hide Loading Bar and show Data
 						$scope.loading = false;
@@ -57,6 +63,7 @@ angular.module('weatherApp', [])
 						// Success
 						$scope.city = weatherData.city;
 						$scope.temperature = weatherData.temperature + "º " + tempUnit;
+						temp = weatherData.temperature;
 						$scope.condition = weatherData.condition;
 						// Hide Loading Bar and show Data
 						$scope.loading = false;
@@ -78,14 +85,29 @@ angular.module('weatherApp', [])
 	}
 
 	$scope.changeUnit = function(unit){
+
 		// Change button class for selected button
+		if (unit == "fahrenheit") {
+			// Change button class for selected button
+			$scope.fahrenheitClass = "btn btn-primary";
+			$scope.celciusClass = "btn btn default";
+			if (tempUnit == "C"){
+				tempUnit = "F";
+				$scope.temperature = temp + "º " + tempUnit;
+			}
 
-
-
+		} else if (unit == "celcius") {
+			// Change button class for selected button
+			$scope.fahrenheitClass = "btn btn-default";
+			$scope.celciusClass = "btn btn-primary";
+			if (tempUnit == "F"){
+				var tempCelcius = (temp - 32) * 0.555555555;
+				tempCelcius = Math.round(tempCelcius);
+				tempUnit = "C";
+				$scope.temperature = tempCelcius + "º " + tempUnit;
+			}	
+		}
 	}
-
-
-
 
 });
 
